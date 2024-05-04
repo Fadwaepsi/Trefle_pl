49da84bf6c9034d8555f8cbca1c3d1dd4f7dc5ab
Fadwa Talik <fadwa.bayoudh@ecoles-epsi.net>
2024-05-04 15:44:16 +0200

    📦 NEW: Recuperer différentes variétés de noix de coco

---
+++ Coconut_species.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://trefle.io/api/v1/species/search?q=coconut&token=u6eaoskjnupYOMtNXHXNJbi_5uUb1D9boacy8Rx4rrs"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
